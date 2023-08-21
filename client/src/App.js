88e31d9b472bed49966b4c9435e5b46e9e682eed
jtrieu1998 <43070404+jtrieu1998@users.noreply.github.com>
2023-08-20 22:42:38 -0500

    WIP need to display middle deck

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,60 @@ const socket = io.connect("http://localhost:3001", {
 })
 
 function App() {
+  const [inLobby, setInLobby] = useState(false)
   const [inGame, setInGame] = useState(false)
+  const [middleDeck, setMiddleDeck] = useState([])
+  const [pid, setPid] = useState(0)
+
+  // TODO: function to check slapability
 
   const addPlayer = () => {
-    
     socket.emit("add_player")
     console.log("Adding player")
-    setInGame(true)
+    setInLobby(true)
   }
 
   const addSpectator = () => {
     socket.emit("add_spectator")
-    setInGame(true)
+    setInLobby(true)
   }
 
+  // For testing, to delete
   const showDeck = () => {
     socket.emit("show_deck")
   }
 
-  if(!inGame){
+  const startGame = () => {
+    socket.emit("start_game")
+    setInGame(true)
+  }
+
+  const playCard = (pid_) => {
+    socket.emit("play_card", (pid_))
+  }
+
+  useEffect(() => {
+    socket.on("player_id", (pid_) => {
+      setPid(pid_)
+    })
+
+    socket.on("game_started", (numPlayers) => {
+      setInGame(true)
+    })
+
+    // TODO: use pid for card count
+    socket.on("card_played", (pid, cardPlayed) => {
+      setMiddleDeck([
+        ...middleDeck,
+        cardPlayed
+      ])
+      console.log(middleDeck, cardPlayed)
+      // TODO: add function to check slapability
+    })
+
+  }, [socket])
+
+  if(!inLobby){
     return ( 
       <div className="App">
         <button onClick={addPlayer}>Connect to play</button>
@@ -33,12 +68,22 @@ function App() {
         <button onClick={showDeck}>SHOW DECK</button>
       </div>
     );
-  } else {
+  } else if(!inGame) {
     return ( 
       <div className="App">
-        <h1>Welcome to the game!</h1>
+        <h1>Welcome to the lobby!</h1>
+        <button onClick={startGame}>Start Game</button>
       </div>
     );
+  } else {
+    return (
+      <div className="App">
+        <h1>Welcome to the game!</h1>
+        <button onClick={() => playCard(pid)}>Play Card</button>
+        {/* <button onClick={player_slap}>Slap</button> */}
+        <div>HERE IS THE MIDDLE DECK: {middleDeck[0]}</div>
+      </div>
+    )
   }
   
 }
--- server/index.js
+++ server/index.js
@@ -18,10 +18,13 @@ const io = new Server(server, {
 
 // Game logic starts here
 let players = new Set()
-let gameStarted = false
+// let gameStarted = false
 let ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 let suits = ['s','c','d','h']
-
+let hands
+let middleDeck = []
+let turn = 0
+let slapable = false
 
 const generateDeck = () => {
   let deck = []
@@ -37,8 +40,7 @@ const generateDeck = () => {
   return deck
 }
 
-const shuffle = () => {
-  let deck = generateDeck()
+const shuffle = (deck) => {
   let card1, card2, temp
 
   for( let i = 0; i < 1000; i++){
@@ -46,16 +48,29 @@ const shuffle = () => {
     card2 = Math.floor((Math.random() * deck.length));
     temp = deck[card1];
 
-    // console.log(deck[card1],card1,deck[card2],card2)
     deck[card1] = deck[card2]
     deck[card2] = temp
-    // console.log(deck)
   }
   
   return deck
 }
 
-let deck = shuffle();
+const dealCards = (numPlayers, deck) => {
+  let hands = []
+
+  for(let h = 0; h < numPlayers; h++){
+    hands[h] = []
+  }
+  let h = 0
+  for(let c = 0; c < deck.length-42; c++){
+    if(h > numPlayers-1){
+      h = 0
+    }
+    hands[h++].push(deck[c])
+  }
+
+  return hands
+} 
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`)
@@ -64,6 +79,7 @@ io.on("connection", (socket) => {
   socket.on("add_player", () => {
     players.add(socket.id)
     console.log(players)
+    socket.broadcast.emit("player_id", (players.size-1))
   })
 
   // Player closes tab
@@ -72,11 +88,46 @@ io.on("connection", (socket) => {
     players.delete(socket.id)
     console.log("Player removed: ", socket.id);
     console.log(players)
+
+    // If all players disconnect reset game
+    if(players.size == 0){
+      middleDeck = []
+      turn = 0
+    }
   });
 
+  // For testing, to delete
   socket.on("show_deck", () => {
     console.log(deck)
   })
+
+  socket.on("start_game", () => {
+    let numPlayers = players.size
+    hands = dealCards(numPlayers, shuffle(generateDeck()))
+    console.log(hands)
+    socket.broadcast.emit("game_started", (numPlayers))
+  })
+
+  socket.on("play_card", (pid) => {
+    console.log("Card was played")
+    if(pid == turn){
+      let cardPlayed = hands[pid].shift()
+      socket.emit("card_played", (pid, cardPlayed))
+      middleDeck.push(cardPlayed)
+      console.log(middleDeck)
+      turn++
+      if(turn > players.size-1){
+        turn = 0
+      }
+      // emit pid for card count feature later
+      
+    } else {
+      // do nothing
+    }
+  })
+
+  // TODO: socket.on slap
+  //   input = slapability, timeToInput, pid
 })
 
 server.listen(3001, () => {
